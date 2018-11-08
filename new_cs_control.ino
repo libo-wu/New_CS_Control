cddd6dd03596fb0fefba099b58644e7f04084cfd
libo.wu1992 <libo.wu1992@gmail.com>
2018-11-08 17:55:28 -0600

    Use Interrupt

--- new_cs_control.ino
+++ new_cs_control.ino
@@ -29,10 +29,12 @@ Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 int samples_per_move = 30;
 int opticalpin = 2;  //use pin 2 (D2) as external interrupt
-int singlestep=int(STEPS_PER_OUTPUT_REVOLUTION/9);  //step to take to move one mask 40 degrees
-int initmoves=256;
+int step2take[9]={228,228,228,228,228,228,228,228,228};  //step to take to move one mask 40 degrees
+int initmoves=1024;
 int step4init=int(STEPS_PER_OUTPUT_REVOLUTION/initmoves); //in initalization, the tolarance of each steps to take is 2048/256=8
 int incoming;
+volatile bool motorflag;  //motorflag=true: stepper motor is running, false: is stopped
+volatile int initpos=0;
 
 // setup
 void setup(){
@@ -40,58 +42,102 @@ void setup(){
   BTserial.println("Connected to BT.");
   mlx.begin();  
   pinMode(opticalpin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   //cli();
 }
 
 //loop
 void loop(){
-
+  
   if (BTserial.available()) {
     // read the incoming:
     incoming = BTserial.read();
+    //********************************//
+    // Method 1: divide 2048 steps into small steps(8steps), 
+    // for each step, poll optical pin untill it is blocked
+    //********************************//
+//    if(incoming==49){ //ascii(49)=1
+//      BTserial.println(incoming);
+//      BTserial.println("enter init");
+//      my_stepper.setSpeed(1000); 
+//      for(int i=0;i<initmoves;i++){
+//        int readval=digitalRead(opticalpin);
+//        if(readval==HIGH){
+//          break;
+//        }
+//        else{
+//          my_stepper.step(step4init);
+//        }
+//      }
+//    return; 
     
+    //********************************//
+    // Method 2: use external interrupt, 
+    //********************************//
     if(incoming==49){ //ascii(49)=1
       BTserial.println(incoming);
-      Initialization();
+      BTserial.println("enter init");
+      
+      my_stepper.setSpeed(1000);                               
+      motorflag=true;
+      BTserial.print("motorflag is:"); BTserial.println(motorflag);
+      
+      noInterrupts ();
+      attachInterrupt (0, StopStepper, RISING);  
+      interrupts ();
+      
+      delay(1000);  
+      digitalWrite(LED_BUILTIN, LOW); 
+      for (int i=0;i<2*initmoves;i++){
+        if(motorflag){
+          my_stepper.step(step4init);
+        }
+        else{
+          BTserial.println("command 1: arrive init");
+          break;
+        }
+      }
+      detachInterrupt(0);
+      return;
     }
     
     if(incoming==50){ //ascii(50)=2
+      digitalWrite(LED_BUILTIN, LOW); 
       BTserial.println(incoming);
       my_stepper.setSpeed(1000);
-      for (int j=0;j<1;j++){ //record 10 rounds
+      for (int j=0;j<3;j++){ //record 10 rounds
+        findInitPos();
         for (int i=0;i<9;i++){
-          my_stepper.step(singlestep);
-          delay(1000);
           for (int j=0;j<samples_per_move;j++){   //samples per move is 30, each will have a delay of 25ms, total 750ms+250ms=1s
             BTserial.print(i); BTserial.print(",");BTserial.print(mlx.readObjectTempC()); BTserial.print(",");BTserial.println(mlx.readAmbientTempC());
             delay(25);
           }
+          my_stepper.step(step2take[i]+1);
+          delay(1000);
         }
       }
       Serial.println("***********");
-    }
-    
+    }   
   }
-
-}
-
-void Initialization(){
-  // poll pin2 until the value is high (means that the optical endstop is blocked), then immediately return.
-  // meanwhile rotating
-    my_stepper.setSpeed(1000);                               
-    BTserial.println("enter init");
-    delay(1000);
-    attachInterrupt (0, StopStepper, RISING);  
-    delay(1000);  
-    my_stepper.step(STEPS_PER_OUTPUT_REVOLUTION);   //stepper motor rotates one round
-    detachInterrupt(0);
-    return;
 }
 
 void StopStepper(){
-  BTserial.println("enter ISR");
-  my_stepper.setSpeed(1000);
+  motorflag=false;
 }
 
+void findInitPos(){ //run this function for every turn
+  my_stepper.setSpeed(1000);                               
+  for (int i=0;i<initmoves;i++){
+    if(digitalRead(opticalpin)==LOW){
+      my_stepper.step(step4init);
+    }
+    else{
+      BTserial.println("command 2: arrive init");
+      break;
+    }
+  }
+  delay(2000);
+  return;
+}
 
 
